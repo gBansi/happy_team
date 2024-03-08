2b9310bb968e55ea0c1adb1c43ec9ac4344a9454
ghetiyabansi <bansip464@gmail.com>
2024-03-08 12:06:21 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ function App() {
      <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
