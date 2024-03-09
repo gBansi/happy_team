e935a8cd1a705cea1b7d4defe247f3f55c6e48ed
bluesoltech-kunal <dev.kunal.bluesoltech@gmail.com>
2024-03-09 14:53:31 +0530

    bug removal lag

--- src/components/What_can_do.jsx
+++ src/components/What_can_do.jsx
@@ -1,76 +1,110 @@
-import React from 'react'
-import slide1 from '../assets/img/slide1.jpg'
-import { FaUserTie, FaMedal, FaDollarSign, FaCalendarAlt } from 'react-icons/fa';
+import React from "react";
+import slide1 from "../assets/img/slide1.jpg";
+import {
+  FaUserTie,
+  FaMedal,
+  FaDollarSign,
+  FaCalendarAlt,
+} from "react-icons/fa";
 import { FaArrowCircleRight } from "react-icons/fa";
-
+import { Link } from "react-router-dom";
 
 const features = [
-    {
-        Icon: FaUserTie,
-        title: 'professional coach',
-        description: 'we bring the right people together to challenge established thinking.'
-    },
-    {
-        Icon: FaMedal,
-        title: 'experienced',
-        description: 'we bring the right people together to challenge established thinking.'
-    },
-    {
-        Icon: FaDollarSign,
-        title: 'affordable price',
-        description: 'we bring the right people together to challenge established thinking.'
-    },
-    {
-        Icon: FaCalendarAlt,
-        title: 'flexible scheduling',
-        description: 'we bring the right people together to challenge established thinking.'
-    }
+  {
+    Icon: FaUserTie,
+    title: "professional coach",
+    description:
+      "we bring the right people together to challenge established thinking.",
+  },
+  {
+    Icon: FaMedal,
+    title: "experienced",
+    description:
+      "we bring the right people together to challenge established thinking.",
+  },
+  {
+    Icon: FaDollarSign,
+    title: "affordable price",
+    description:
+      "we bring the right people together to challenge established thinking.",
+  },
+  {
+    Icon: FaCalendarAlt,
+    title: "flexible scheduling",
+    description:
+      "we bring the right people together to challenge established thinking.",
+  },
 ];
 
 const What_can_do = () => {
-    return (
-        <div className='my-[70px]'>
-            <div className='flex flex-col xl:flex-row items-center justify-between gap-2 py-10'>
-                <h1 className='text-[#26173F] text-lg xl:text-5xl font-bold text-center'>What Can We <span className='text-[#27BDEE]'>Do?</span></h1>
-                <p className='text-xl xl:w-[60%]'>Creating Extraordinary Event Is Our Specialty. Innovative Designs, Entertainment And Customized Theme Decor, Glamorous, Event Plan Delivers Resonance To The WOW Factor While Saving Our Clients Valuable Time And Money</p>
-            </div>
-            <div className='flex flex-col xl:flex-row items-start justify-center gap-2 py-10'>
-                {/* Image container to control the space around the image */}
-                <div className="flex flex-col justify-start items-start xl:w-[40%]">
-                    <img src={slide1} alt="Description" className='rounded-full sm:h-[400px] sm:w-[400px] w-[300px] h-[300px]' />
-                </div>
-
-                {/* Content */}
-                <div className='xl:w-[60%] text-xl text-start xl:text-left'>
-                    <div className="bg-white text-gray-800 p-6">
-                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                            {features.map((feature, index) => (
-                                <div key={index} className="flex flex-col items-center xl:items-start text-center xl:text-start">
-                                    <div className='h-fit w-fit p-2 bg-[#27BDEE]'>
-                                        <feature.Icon className="text-white text-5xl hover:text-[#26173F]" />
-                                    </div>
-                                    <h2 className="text-xl font-bold mb-2 text-start">{feature.title}</h2>
-                                    <p>{feature.description}</p>
-                                </div>
-                            ))}
-                        </div>
-                        <hr className='my-4 h-1 bg-[#26173F] border-none' />
-                        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 mx-auto">
-                            <img src={slide1} className='h-[100px] w-[100px] rounded-full overflow-hidden mx-auto'></img>
-                            <div className='mx-auto'>
-                                <h1 className='text-[#26173F] text-lg xl:text-2xl font-bold text-start'>vishnu kamaliya</h1>
-                                <p className='text-xl'>ceo happy tame</p>
-                            </div>
-                            <button className='flex items-center justify-center bg-[#26173F] h-fit w-fit text-white rounded-full p-2 space-x-2 mx-auto'>
-                                <FaArrowCircleRight />
-                                <span>About Us</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="my-[70px]">
+      <div className="flex flex-col xl:flex-row items-center justify-between gap-2 py-10">
+        <h1 className="text-[#26173F] text-lg xl:text-5xl font-bold text-center">
+          What Can We <span className="text-[#27BDEE]">Do?</span>
+        </h1>
+        <p className="text-xl xl:w-[60%]">
+          Creating Extraordinary Event Is Our Specialty. Innovative Designs,
+          Entertainment And Customized Theme Decor, Glamorous, Event Plan
+          Delivers Resonance To The WOW Factor While Saving Our Clients Valuable
+          Time And Money
+        </p>
+      </div>
+      <div className="flex flex-col xl:flex-row items-start justify-center gap-2 py-10">
+        {/* Image container to control the space around the image */}
+        <div className="flex flex-col justify-start items-start xl:w-[40%]">
+          <img
+            src={slide1}
+            alt="Description"
+            className="rounded-full sm:h-[400px] sm:w-[400px] w-[300px] h-[300px]"
+          />
         </div>
-    )
-}
 
-export default What_can_do
+        {/* Content */}
+        <div className="xl:w-[60%] text-xl text-start xl:text-left">
+          <div className="bg-white text-gray-800 p-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              {features.map((feature, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col items-center xl:items-start text-center xl:text-start"
+                >
+                  <div className="h-fit w-fit p-2 bg-[#27BDEE]">
+                    <feature.Icon className="text-white text-2xl hover:text-[#26173F]" />
+                  </div>
+                  <h2 className="text-xl font-bold mb-2 text-start">
+                    {feature.title}
+                  </h2>
+                  <p>{feature.description}</p>
+                </div>
+              ))}
+            </div>
+            <hr className="my-4 h-[1px] bg-[#26173F] border-none" />
+            <div className="flex items-center gap-10">
+              <div className="flex items-center gap-5">
+                <img
+                  src={slide1}
+                  className="h-[100px] w-[100px] rounded-full overflow-hidden mx-auto"
+                ></img>
+                <div className="mx-auto">
+                  <h1 className="text-[#26173F] text-lg xl:text-2xl font-bold text-start">
+                    vishnu kamaliya
+                  </h1>
+                  <p className="text-xl">ceo happy tame</p>
+                </div>
+              </div>
+              <Link to="https://vishnukamaliya.com">
+                <button className="flex items-center justify-center bg-[#26173F] h-fit w-fit text-white rounded-full p-2 space-x-2 mx-auto">
+                  <FaArrowCircleRight />
+                  <span>About Us</span>
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default What_can_do;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,92 +1,120 @@
-import React, { useState, useEffect } from 'react';
-import slide1 from '../assets/img/slide1.jpg'
-import slide2 from '../assets/img/slide2.jpg'
-import slide3 from '../assets/img/slide3.jpg'
-import What_can_do from '../components/What_can_do';
-import About from '../components/About';
-import Logo from '../components/Logo';
-import Past_event from '../components/Past_event';
-import Services from '../components/Services';
+import React, { useState, useEffect } from "react";
+import slide1 from "../assets/img/slide1.jpg";
+import slide2 from "../assets/img/slide2.jpg";
+import slide3 from "../assets/img/slide3.jpg";
+import What_can_do from "../components/What_can_do";
+import Logo from "../components/Logo";
+import Past_event from "../components/Past_event";
+import Services from "../components/Services";
+import AboutComp from "../components/AboutComp";
 
 const images = [
-    { imgSrc: slide1, alt: 'First Image Description', title: 'First Image Title', description: 'This is the first image description.' },
-    { imgSrc: slide2, alt: 'Second Image Description', title: 'Second Image Title', description: 'This is the second image description.' },
-    { imgSrc: slide1, alt: 'First Image Description', title: 'First Image Title', description: 'This is the first image description.' },
+  {
+    imgSrc: slide1,
+    alt: "First Image Description",
+    title: "First Image Title",
+    description: "This is the first image description.",
+  },
+  {
+    imgSrc: slide2,
+    alt: "Second Image Description",
+    title: "Second Image Title",
+    description: "This is the second image description.",
+  },
+  {
+    imgSrc: slide1,
+    alt: "First Image Description",
+    title: "First Image Title",
+    description: "This is the first image description.",
+  },
 
-    // Add more images and content as needed
+  // Add more images and content as needed
 ];
 const imageData = [
-    {
-        src: slide1,
-        alt: 'First Image',
-        title: 'Title 1',
-    },
-    {
-        src: slide2,
-        alt: 'Second Image',
-        title: 'Title 2',
-    },
-    {
-        src: slide3,
-        alt: 'Third Image',
-        title: 'Title 3',
-    },
-    // Add more images as needed
+  {
+    src: slide1,
+    alt: "First Image",
+    title: "Title 1",
+  },
+  {
+    src: slide2,
+    alt: "Second Image",
+    title: "Title 2",
+  },
+  {
+    src: slide3,
+    alt: "Third Image",
+    title: "Title 3",
+  },
+  // Add more images as needed
 ];
 const Home = () => {
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    useEffect(() => {
-        // Change the background image and content every 5 seconds
-        const interval = setInterval(() => {
-            setCurrentImageIndex((currentImageIndex + 1) % images.length);
-        }, 2000);
+  useEffect(() => {
+    // Change the background image and content every 5 seconds
+    const interval = setInterval(() => {
+      setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    }, 2000);
 
-        return () => clearInterval(interval);
-    }, [currentImageIndex]);
+    return () => clearInterval(interval);
+  }, [currentImageIndex]);
 
-    return (
-        <div className=''>
-            <div
-                className=" flex items-center rounded-3xl h-[500px] xl:h-[700px] overflow-hidden mx-auto my-4 text-white py-11"
-                style={{
-                    backgroundImage: `url(${images[currentImageIndex].imgSrc})`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center'
-                }}
-            >
-                <div className="p-4 bg-black bg-transparent rounded ml-4 md:ml-10 lg:ml-16">
-                    <h1 className="text-3xl font-bold">{images[currentImageIndex].title}</h1>
-                    <p>{images[currentImageIndex].description}</p>
-                </div>
-            </div>
-            <div className=''>
-                <h1 className='text-[#26173F] text-lg xl:text-5xl font-bold text-center my-[70px]'>Up Coming <span className='text-[#27BDEE] text-lg xl:text-5xl font-bold'>Event</span></h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-between mx-auto">
-                    {imageData.map((item, index) => (
-                        <div key={index} className="relative group w-[250px] xl:w-[400px] h-[250px] xl:h-[400px] mx-auto">
-                            {/* Image */}
-                            <img src={item.src} alt={item.alt} className="w-full h-full rounded-lg object-cover" />
-
-                            {/* Blue Overlay */}
-                            <div className="absolute inset-0 bg-gray-50 opacity-75 group-hover:opacity-0 transition duration-300 ease-in-out"></div>
-
-                            {/* Overlay Content */}
-                            <div className="absolute inset-0 flex items-center justify-center flex-col px-4 text-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
-                                <h3 className="text-3xl font-bold text-white">{item.title}</h3>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-
-            </div>
-            <What_can_do />
-            <About />
-            <Logo />
-            <Past_event />
-            <Services />
+  return (
+    <div className="">
+      <div
+        className=" flex items-center rounded-3xl h-[500px] xl:h-[700px] overflow-hidden mx-auto my-4 text-white py-11"
+        style={{
+          backgroundImage: `url(${images[currentImageIndex].imgSrc})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="p-4 bg-black bg-transparent rounded ml-4 md:ml-10 lg:ml-16">
+          <h1 className="text-3xl font-bold">
+            {images[currentImageIndex].title}
+          </h1>
+          <p>{images[currentImageIndex].description}</p>
         </div>
-    )
-}
+      </div>
+      <div className="">
+        <h1 className="text-[#26173F] text-lg xl:text-5xl font-bold text-center my-[70px]">
+          Up Coming{" "}
+          <span className="text-[#27BDEE] text-lg xl:text-5xl font-bold">
+            Event
+          </span>
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-between mx-auto">
+          {imageData.map((item, index) => (
+            <div
+              key={index}
+              className="relative group w-[250px] xl:w-[400px] h-[250px] xl:h-[400px] mx-auto"
+            >
+              {/* Image */}
+              <img
+                src={item.src}
+                alt={item.alt}
+                className="w-full h-full rounded-lg object-cover"
+              />
 
-export default Home
+              {/* Blue Overlay */}
+              <div className="absolute inset-0 bg-gray-50 opacity-75 group-hover:opacity-0 transition duration-300 ease-in-out"></div>
+
+              {/* Overlay Content */}
+              <div className="absolute inset-0 flex items-center justify-center flex-col px-4 text-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                <h3 className="text-3xl font-bold text-white">{item.title}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <What_can_do />
+      <AboutComp />
+      <Logo />
+      <Past_event />
+      <Services />
+    </div>
+  );
+};
+
+export default Home;
